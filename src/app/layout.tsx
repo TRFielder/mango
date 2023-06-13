7d7ee6c746066222b5b041650d4edc9c4ce13ad5
Tom Fielder <tom.fielder@npl.co.uk>
2023-06-13 23:33:21 +0100

    disable metadata export temporarily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,12 @@ import theme from "./theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+// We can't use this whilst we "use client" in this component. Will need to look into a solution for the "use client" issue
+/*export const metadata = {
 	title: "Telemeasure",
 	description: "The television metrology application",
 };
-
+*/
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
